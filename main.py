e86bdb2e841b67802918c19d890eb6189020e939
GiorgioFranco715 <138186314+GiorgioFranco715@users.noreply.github.com>
2023-07-03 19:14:34 +0300

    urok 4

---
+++ main.py
@@ -0,0 +1,50 @@
+if __name__ == "__main__":
+    print('test')
+    #names = "Юрій"
+    #surname = "Юськів"
+    #fathername = "Михайлович"
+    #
+    #
+    #
+    # print(f"{surname},{name[0].upper},{fathername[0].upper}")
+    #set
+    #arr_surnames = {"Doe","Lee","Chan"}
+    #arr_surnames.add('test')
+   # print(arr_surnames)
+   # print(user_data['pib'])
+    #user_data =
+     #примітивні дані
+    # a = 1
+    # b = a
+    # b += 1
+    # print(a,b)
+    #Складні дані
+    # a = [1]
+    # b = a
+    # b += [1]
+    # print(a,b)
+
+    users = [
+        {
+            "name":"John",
+            "age": 31
+        },
+        {
+            "name": "Jane",
+            "age": 27
+        },{
+            "name": "Jack",
+            "age": 15
+        },{
+            "name": "Jeff",
+            "age": 40
+        }
+    ]
+    # del users[0]
+    # del users[2]
+    # del users[0]
+    # del (users[0])
+
+    #print(users[2]["name"],users[2]["age"])
+
+    temp_age = 
